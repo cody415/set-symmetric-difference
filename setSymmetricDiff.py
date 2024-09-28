747a5536900fb7402c6896359d2d682be267cda6
Aparna1407 <aparnadesai7@gmail.com>
2024-09-29 00:33:58 +0530

    added

--- setSymmetricDiff.py
+++ setSymmetricDiff.py
@@ -1,3 +1,7 @@
 set_A = {1, 2, 3, 4, 5}
 set_B = {6, 7, 3, 9, 4}
 print(set_A.symmetric_difference(set_B))
+
+Set1 = {'blue', 'green'} 
+Set2 = {'blue', 'yellow'}
+print(Set1.symmetric_difference(Set2))
